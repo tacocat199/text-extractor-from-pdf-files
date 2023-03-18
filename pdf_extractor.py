403c5c5094c0d04df7ec391046a90a5d8281cf94
tacocat199 <farteezy19@gmail.com>
2023-03-18 21:39:44 +0700

    Okayeg

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -1,11 +1,11 @@
 import PyPDF2
 
-pdf_file = open('F:\pdf books\Spirituals, Psychology Books\Homer_ The Odyssey ( PDFDrive.com ).pdf', 'rb')
-pdf_reader = PyPDF2.PdfileReader(pdf_file)
+pdf_file = open('Homer_The Odyssey_( PDFDrive.com).pdf', 'rb')
+pdf_reader = PyPDF2.PdfReader(pdf_file)
 
 text = ''
-for page in range(pdf_reader.getNumPages()):
-    text += pdf_reader.getPage(page).extractText()
+for page in range(len(pdf_reader.pages)):
+    text += pdf_reader.pages[page].extract_text()
 
 with open('input.text', 'w') as f:
     f.write(text)
